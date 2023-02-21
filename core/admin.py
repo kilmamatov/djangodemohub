d3460e85d5560eade739973f7fdefb4e10679a00
kilmamatov <117253418+kilmamatov@users.noreply.github.com>
2023-02-21 14:10:22 +0500

    #1

--- core/admin.py
+++ core/admin.py
@@ -2,8 +2,13 @@ from django.contrib import admin
 from core import models
 
 
+class TodoResultInline(admin.TabularInline):
+    model = models.TodoResult
+
+
 @admin.register(models.Todo)
 class Todo(admin.ModelAdmin):
-    list_display = ('name', 'id', 'priority', 'created', 'update', 'done', 'duration')
-    search_fields = ('name', 'id')
-    list_filter = ('created', 'done')
+    inlines = (TodoResultInline,)
+    list_display = ('name', 'done',)
+    search_fields = ('name',)
+
--- core/models.py
+++ core/models.py
@@ -1,34 +1,7 @@
 import datetime
 from django.contrib.auth import get_user_model
 from django.db import models
-from django.utils import timezone
 from django.utils.timezone import now
-
-
-class Person(models.Model):
-    name = models.CharField('Имя', max_length=15)
-    phone = models.IntegerField('Номер телефона')
-
-    class Meta:
-        verbose_name = 'Заказчик'
-        verbose_name_plural = 'Заказчики'
-
-    def __str__(self):
-        return self.name
-
-
-class TodoList(models.Model):
-    title = models.CharField('Имя', max_length=25)
-    content = models.TextField('Задачи', blank=True)
-    created = models.DateField('Дата начала', default=timezone.now().strftime("%Y-%m-%d"))
-    due_date = models.DateField('Дата завершения', default=timezone.now().strftime("%Y-%m-%d"))
-
-    class Meta:
-        verbose_name = 'Пользователь'
-        verbose_name_plural = 'Пользователи'
-
-    def __str__(self):
-        return self.title
 
 
 User = get_user_model()
@@ -36,15 +9,15 @@ User = get_user_model()
 
 class Todo(models.Model):
     user = models.ForeignKey(User, verbose_name='Пользователь', on_delete=models.CASCADE, related_name='todo')
-    name = models.CharField('Название', unique=True, max_length=255, blank=True,)
+    name = models.CharField('Название', max_length=25, blank=True, unique=True,)
     content = models.TextField('Описание', help_text='Какая либо подсказка, если имеет смысл')
     priority = models.IntegerField('Приоритет сортировки', default=1)
     done = models.DateTimeField('Выполнено', null=True, blank=True)  # BooleanField так же можно использовать
-    created = models.DateTimeField('Создан', auto_now_add=True,)
+    created = models.DateTimeField('Создан', auto_now_add=True)
     update = models.DateTimeField('Обновлен', auto_now=True)
 
     class Meta:
-        verbose_name = 'Дела'
+        verbose_name = 'Дело'
         verbose_name_plural = 'Список дел'
 
     def __str__(self):
@@ -52,3 +25,8 @@ class Todo(models.Model):
 
     def duration(self) -> datetime.timedelta:
         return now() - self.created
+
+
+class TodoResult(models.Model):
+    item = models.OneToOneField(Todo, on_delete=models.CASCADE)
+    image = models.ImageField()
